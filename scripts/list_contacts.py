b9b910ab8f4d89eaa76a4f9cf0ee17f7ad0a9dcd
Todd Clarke <77644845+modrocko@users.noreply.github.com>
2025-06-25 11:11:47 -0700

    Update list_contacts.py

--- scripts/list_contacts.py
+++ scripts/list_contacts.py
@@ -9,6 +9,7 @@ import utils
 folder = os.environ["contact_folder"]
 ext = os.environ["file_extension"].strip()
 query = sys.argv[1].strip().lower() if len(sys.argv) > 0 else ""
+rowcount = 0
 
 # === Parse query using utils ===
 is_or, query_terms = utils.parse_query(query)
@@ -25,27 +26,6 @@ def get_field(field, content):
     return ""
 
 items = []
-
-if query:
-    items.append({
-        "title": "Choose command...",
-        "subtitle": "↵ Save this search ∙ ⌘ Update contacts ∙ ⌥ Open contacts",
-        "arg": query,
-        "icon": { "path": "info.png" },
-        "variables": { "action": "save_search" },
-        "mods": {
-            "cmd": {
-                "subtitle": "⌘ Update contacts in this list",
-                "variables": { "query": query }
-            },
-            "alt": {
-                "subtitle": "⌥ Open contacts in this list",
-                "arg": query,
-                "variables": { "query": query }
-            }
-
-        }
-    })
 
 # === If folder not found ===
 if not os.path.exists(folder):
@@ -78,6 +58,7 @@ else:
             if match:
                 icon = utils.get_icon_for_tag(fields.get("Tags", ""))
                 matched = True
+                rowcount += 1
                 items.append({
                     "title": name,
                     "subtitle": subtitle,
@@ -104,6 +85,29 @@ else:
             "subtitle": f"Query: {query}",
             "valid": False
         })
+
+# === Summary row at the top (did this to get rowcount before inserting this row) ===
+if query:
+    noun = "contact" if rowcount == 1 else "contacts"
+    items.insert(0, {
+        "title": f"{rowcount} {noun}",
+        "subtitle": "↵ Save this search ∙ ⌘ Update contacts ∙ ⌥ Open contacts",
+        "arg": query,
+        "icon": { "path": "info.png" },
+        "variables": { "action": "save_search" },
+        "mods": {
+            "cmd": {
+                "subtitle": "⌘ Update contacts in this list",
+                "variables": { "query": query }
+            },
+            "alt": {
+                "subtitle": "⌥ Open contacts in this list",
+                "arg": query,
+                "variables": { "query": query }
+            }
+        }
+    })
+
 
 # === Output JSON for Alfred ===
 print(json.dumps({ "items": items }))
